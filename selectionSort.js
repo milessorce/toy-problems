252be0ff8f9efe42703d2b6322b3712ab144ab59
Miles Sorce <m.sorce@gmail.com>
2017-10-14 11:18:22 -0700

    Add selection sort

---
+++ selectionSort.js
@@ -0,0 +1,20 @@
+const selectionSort = arr => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let temp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = temp;
+    }
+  }
+  return arr;
+};
+
+let a = [24, 2, 44, 19, 12, 14, 34, 1];
+let output = selectionSort(a);
+console.log(output);
