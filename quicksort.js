91101e486aa51c28065060da409facadd76a3c7a
Miles Sorce <m.sorce@gmail.com>
2017-10-16 09:26:19 -0700

    Optimize quicksort by moving pivot to middle

--- quicksort.js
+++ quicksort.js
@@ -1,20 +1,23 @@
 const quicksort = arr => {
-  if (arr.length === 0) {
-    return [];
+  if (arr.length <= 1) {
+    return arr;
   }
-  let pivot = arr[0];
+  let pivot = arr[Math.floor(arr.length / 2)];
   let right = [];
   let left = [];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] <= pivot) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
       left.push(arr[i]);
-    } else {
+    } else if (arr[i] > pivot) {
       right.push(arr[i]);
     }
   }
-  return quicksort(left).concat(pivot, quicksort(right));
+  return quicksort(left).concat(pivot).concat(quicksort(right));
 };
 
-let a = [22, 44, 11, 13, 1, 141, 12, 1, 99, 53];
-let output = quicksort(a);
+let list = [];
+for (let i = 10000; i > 0; i--) {
+  list.push(i);
+}
+let output = quicksort(list);
 console.log(output);
