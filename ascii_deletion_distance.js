cc0e4d5fc56a92749693abb8ab567dda9129fdd3
Miles Sorce <m.sorce@gmail.com>
2017-10-17 18:27:12 -0700

    Add solution for ascii_deletion_distance

---
+++ ascii_deletion_distance.js
@@ -0,0 +1,35 @@
+function ascii_deletion_distance(str1, str2) {
+  if (str1 === str2) {
+    return 0;
+  }
+  let letterMap1 = str1.split('').reduce((map, char) => {
+    map[char] = map[char] ? map[char] + 1 : 1;
+    return map;
+  }, {});
+  let letterMap2 = str2.split('').reduce((map, char) => {
+    map[char] = map[char] ? map[char] + 1 : 1;
+    return map;
+  }, {});
+  let distance = 0;
+  for (let char in letterMap1) {
+    if (letterMap2[char] && letterMap1[char] !== letterMap2[char]) {
+      let num = Math.abs(letterMap1[char] - letterMap2[char]);
+      distance += (char.charCodeAt(0) * num);
+    } else if (!letterMap2[char]) {
+      let num = letterMap1[char];
+      distance += (char.charCodeAt(0) * num);
+    }
+  }
+  for (let char in letterMap2) {
+    if (!letterMap1[char]) {
+      let num = letterMap2[char];
+      distance += (char.charCodeAt(0) * num);
+    }
+  }
+  return distance;
+}
+
+console.assert(ascii_deletion_distance('at', 'at') === 0);
+console.assert(ascii_deletion_distance('at', 'cat') === 99);
+console.assert(ascii_deletion_distance('boat', 'got') === 298);
+console.assert(ascii_deletion_distance('thought', 'sloughs') === 674);
