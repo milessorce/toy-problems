a6d2eabb5e2dd8e7e8de2c35eeaa13f0fe8a2b9e
Miles Sorce <m.sorce@gmail.com>
2017-10-20 11:47:32 -0700

    Improve ascii_deletion_distance solution

--- ascii_deletion_distance.js
+++ ascii_deletion_distance.js
@@ -1,34 +1,32 @@
-function ascii_deletion_distance(str1, str2) {
-  if (str1 === str2) {
-    return 0;
-  }
-  let letterMap1 = str1.split('').reduce((map, char) => {
-    map[char] = map[char] ? map[char] + 1 : 1;
-    return map;
-  }, {});
-  let letterMap2 = str2.split('').reduce((map, char) => {
-    map[char] = map[char] ? map[char] + 1 : 1;
-    return map;
-  }, {});
-  let distance = 0;
-  for (let char in letterMap1) {
-    if (letterMap1[char] && letterMap2[char] && letterMap1[char] !== letterMap2[char]) {
-      let num = Math.abs(letterMap1[char] - letterMap2[char]);
-      distance += (char.charCodeAt(0) * num);
-    } else if (letterMap1[char] && !letterMap2[char]) {
-      let num = letterMap1[char];
-      distance += (char.charCodeAt(0) * num);
+const ascii_deletion_distance =(str1, str2) => {
+    if (str1 === str2) {
+        return 0;
     }
-  }
-  for (let char in letterMap2) {
-    if (letterMap2[char] && !letterMap1[char]) {
-      let num = letterMap2[char];
-      distance += (char.charCodeAt(0) * num);
+    const letterMap1 = str1.split('').reduce((map, char) => {
+        map[char] = map[char] ? map[char] + 1 : 1;
+        return map;
+    }, {});
+    const letterMap2 = str2.split('').reduce((map, char) => {
+        map[char] = map[char] ? map[char] + 1 : 1;
+        return map;
+    }, {});
+    let distance = 0;
+    for (let char in letterMap1) {
+        if (letterMap1[char] && letterMap2[char] && letterMap1[char] !== letterMap2[char]) {
+            distance += (char.charCodeAt(0) * Math.abs(letterMap1[char] - letterMap2[char]));
+        } else if (letterMap1[char] && !letterMap2[char]) {
+            distance += (char.charCodeAt(0) * letterMap1[char]);
+        }
     }
-  }
-  return distance;
+    for (let char in letterMap2) {
+        if (letterMap2[char] && !letterMap1[char]) {
+            distance += (char.charCodeAt(0) * letterMap2[char]);
+        }
+    }
+    return distance;
 }
 
+console.assert(ascii_deletion_distance('at', 'at') === 0);
 console.assert(ascii_deletion_distance('at', 'cat') === 99);
 console.assert(ascii_deletion_distance('boat', 'got') === 298);
 console.assert(ascii_deletion_distance('thought', 'sloughs') === 674);
